c0e0729c053a0cbd0118d6e5627eb2e3b3aa1902
gadamer1 <gadamer1@navaer.com>
2019-06-03 03:06:57 +0900

    last

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^5q3o+)ivhmyaetmsfl2itcf!wo=jqpx0s1m3*+q*$1xrqn-lb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
